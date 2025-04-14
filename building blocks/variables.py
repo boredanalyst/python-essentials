7ee9b6098e8e8c8c7a339771b81afcf17d1bfb8a
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2025-04-14 13:56:21 +0800

    Add files via upload

---
+++ building blocks/variables.py
@@ -0,0 +1,13 @@
+# Variables are entities or components that contain a value.
+
+x = 'Problems.'
+y = 9
+
+print("---")
+print("The following are variables.")
+print("This variable contains a string value:")
+print(f'Variable "x" contains the value: {x}')
+print("---")
+print("This contains a number.")
+print(f'The variable "y" contains the value: {y}')
+print(x,y)
