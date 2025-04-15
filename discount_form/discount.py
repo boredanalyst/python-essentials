976706bc03167f09f4589bad58f381dec73d39ea
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2025-04-15 12:46:20 +0800

    Add files via upload

---
+++ discount_form/discount.py
@@ -0,0 +1,9 @@
+price = 0
+discount = 0.10
+
+print("DISCOUNT CALCULATOR")
+print("---")
+
+price = input("How much is the total?: ")
+print("TOTAL DISCOUNT: 10%")
+print(f'DISCOUNTED PRICE: {price + (price * discount)}')
