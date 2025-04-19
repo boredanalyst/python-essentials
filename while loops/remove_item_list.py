10120d5c74372248dae1014d52dd12a8d7acd626
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2025-04-20 04:04:57 +0800

    Add files via upload

---
+++ while loops/remove_item_list.py
@@ -0,0 +1,7 @@
+## We'll remove item from a list one at a time.
+
+colors = ["red","orange","yellow"]
+
+while len(colors) > 0:
+    print(colors)
+    colors.pop()
---
+++ while loops/while.py
@@ -0,0 +1,7 @@
+## While LOOPs
+
+counter = 0
+
+while counter <= 10:
+    print(f'This statement has been printed {counter} times.')
+    counter += 1
